1b0b10e024ff81f71256fbb64fd81396850e3728
Ricardo Maldonado <rimaldonado@servexternos.santandertecnologia.com.ar>
2020-09-03 19:54:21 -0300

    Release of GifExpertApp

---
+++ docs/precache-manifest.2420fd2aede38cf6de9b13d567a54c34.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ee463c58cf6a20b62040d50ffdae661",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2043ce9a25d195ef6b56",
+    "url": "/static/css/main.63bf7673.chunk.css"
+  },
+  {
+    "revision": "bf47ead62ee7f4d81e82",
+    "url": "/static/js/2.d324843d.chunk.js"
+  },
+  {
+    "revision": "25bd1f3957ca44dc547483fd7f7a83fd",
+    "url": "/static/js/2.d324843d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2043ce9a25d195ef6b56",
+    "url": "/static/js/main.b0f3ecde.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.11f14282f54775ffaf92796b986326e8.js"
+  "/precache-manifest.2420fd2aede38cf6de9b13d567a54c34.js"
 );
 
 self.addEventListener('message', (event) => {
